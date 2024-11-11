bfc8d511fd5d291cad23204cbc80ae1ae826df90
Albarracin Mauro 1BAD <mauro.tupac@gmail.com>
2024-11-10 22:50:44 -0300

    First Test
    Se realizo un primer testeo como se recomienda en la pagina oficial de AltUnity Test. El testeo fue exitoso

---
+++ Assets/Editor/MyFirstTest.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+using AltTester.AltTesterUnitySDK.Driver;
+
+public class MyFirstTest
+{
+    private AltDriver altDriver;
+
+    [OneTimeSetUp]
+    public void SetUp()
+    {
+        altDriver = new AltDriver();
+    }
+
+    [OneTimeTearDown]
+    public void TearDown()
+    {
+        altDriver.Stop();
+    }
+
+    [Test]
+    public void TestStartGame()
+    {
+        altDriver.LoadScene("Scene 2 Draggable Panel");
+
+        altDriver.FindObject(By.NAME, "Close Button").Tap();
+        altDriver.FindObject(By.NAME, "Button").Tap();
+
+        var panelElement = altDriver.WaitForObject(By.NAME, "Panel");
+        Assert.IsTrue(panelElement.enabled);
+    }
+}
